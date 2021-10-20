467bdeab88d0e0a432e4415c10347e6c72292223
Rahif M <rahifmanjatha372@gmail.com>
2021-10-20 16:25:11 +0530

    Create misc.sh

---
+++ misc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name RahifM
+git config --global user.email rahifmanjatha372@gmail.com
+
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
+git config --global alias.l 'log --oneline'
+git config --global alias.ck 'checkout'
+
+alias v="vim"
