854406c7f0d82f042e1ed063ab3062abdd7e09f4
Rahif M <rahifmanjatha372@gmail.com>
2021-10-20 16:58:39 +0530

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -7,5 +7,6 @@ git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
 git config --global alias.l 'log --oneline'
 git config --global alias.ck 'checkout'
-
-alias v="vim"
+git config --global alias.d 'diff'
+git config --global alias.s 'status'
+git config --global alias.p 'push'
