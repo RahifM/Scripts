2015bf3bcc6d106804cbd23cec2ca7dd7c0206b8
RahifM <rahifmanjatha372@gmail.com>
2021-10-29 10:29:46 +0530

    Update misc

--- misc.sh
+++ misc.sh
@@ -3,11 +3,13 @@
 git config --global user.name RahifM
 git config --global user.email rahifmanjatha372@gmail.com
 
-git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
-git config --global alias.l 'log --oneline'
+git config --global alias.cp 'cherry-pick'
 git config --global alias.ck 'checkout'
 git config --global alias.d 'diff'
-git config --global alias.s 'status'
+git config --global alias.f 'fetch'
+git config --global alias.l 'log --oneline'
 git config --global alias.p 'push'
+git config --global alias.s 'status'
+
 git config --global credential.helper 'cache --timeout=99999999'
