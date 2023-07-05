13f20f336af328c79bd5667939608c7442e69634
RahifM <rahifmanjatha372@gmail.com>
2023-07-06 04:00:11 +0530

    update

--- misc.sh
+++ misc.sh
@@ -5,20 +5,23 @@ git config --global user.email rahifmanjatha372@gmail.com
 git config --global core.editor vim
 git config --global alias.b 'branch'
 git config --global alias.c 'commit'
+git config --global alias.ca 'commit --amend'
 git config --global alias.cp 'cherry-pick'
+git config --global alias.cpc 'cherry-pick --continue'
+git config --global alias.cps 'cherry-pick --skip'
 git config --global alias.ck 'checkout'
 git config --global alias.d 'diff'
 git config --global alias.f 'fetch'
 git config --global alias.l 'log --oneline'
 git config --global alias.p 'push'
-git config --global alias.r 'revert'
+git config --global alias.pl 'pull'
 git config --global alias.rh 'reset --hard'
-git config --global alias.rr 'revert --no-edit'
+git config --global alias.r 'revert --no-edit'
 git config --global alias.s 'status'
+git config --global alias.rf 'reflog'
 git config --global alias.rmv 'remote -v'
 git config --global alias.rma 'remote add'
 git config --global alias.rmd 'remote remove'
-git config --global alias.pl 'pull'
 git config --global credential.helper store
 git config --global --add --bool push.autoSetupRemote true
 
